0568e7d4d415d80c4b092a680c4a1553e9e3069e
augustinromaniak <augustin.romaniak88@zoznam.sk>
2022-11-25 12:05:21 +0100

    yo

--- Program_25_11/Class1.cs
+++ Program_25_11/Class1.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace Program_25_11
 {
-    internal class Class1
+    internal class Student
     {
+        public string name = "";
+        public int gradeChem = 1;
+        public int gradePhy = 3;
+
+        public double gradeAverage()
+        {
+            return (double)(gradeChem + gradePhy) / 2;
+        }
     }
 }
