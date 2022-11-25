ad8b1bcbc7c49c6776155359612bc69e2087e329
augustinromaniak <augustin.romaniak88@zoznam.sk>
2022-11-25 11:39:00 +0100

    initial commit

---
+++ Program_25_11/Klas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_25_11
+{
+    internal class Klas
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("sup");
+            Console.ReadKey();
+        }
+    }
+}
