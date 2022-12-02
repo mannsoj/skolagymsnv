18e60795d187bcf32c5fd9d9541b0a66e4b0266e
augustinromaniak <augustin.romaniak88@zoznam.sk>
2022-12-02 12:00:22 +0100

    subject pokus dva

--- Program_25_11/Subject.cs
+++ Program_25_11/Subject.cs
@@ -6,7 +6,29 @@ using System.Threading.Tasks;
 
 namespace Program_25_11
 {
-    internal class Class1
+    internal class Subject
     {
+        public Subject(string name, int one, int two, int three)
+        {
+            subjectname = name;
+            grades[0] = one;
+            grades[1] = two;
+            grades[3] = three;
+        }
+
+        private int[] grades = new int[3];
+        private string subjectname = "";
+
+
+
+        public double Average()
+        {
+            double average = 0;
+            foreach(int i in grades)
+            {
+                average += grades[i];
+            }
+            return average/grades.Length;
+        }
     }
 }
