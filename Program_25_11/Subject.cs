6cd9113f69a471bdde452a067be03f241f803345
augustinromaniak <augustin.romaniak88@zoznam.sk>
2022-12-09 11:37:03 +0100

    something

--- Program_25_11/Subject.cs
+++ Program_25_11/Subject.cs
@@ -22,29 +22,44 @@ namespace Program_25_11
         }
 
         //zadaj
-        public void Constructor(string name, int one, int two, int three)
+        public void Constructor(string name, int[] grds)
         {
             subjectname = name;
-            grades[0] = one;
-            grades[1] = two;
-            grades[3] = three;
+            int i = 0;   
+            foreach(var g in grds)
+            {
+                grades[i] = g;
+                i++;
+            }
         }
 
 
         //vytvorit settery/gettery alebo public
         private int[] grades = new int[3];
-        private string subjectname = "";
+        private string subjectname;
 
 
 
-        public double Average()
+        public double getAverage()
         {
+            int i = 0;
             double average = 0;
-            foreach(int i in grades)
+            foreach(int g in grades)
             {
                 average += grades[i];
+                i++;
             }
             return average/grades.Length;
+        }
+        public string getGrades()
+        {
+            string grds;
+            grds = grades[0].ToString();
+            for (int i = 1; i < grades.Length; i++)
+            {
+                grds += "," + grades[i].ToString();
+            }
+            return grds;
         }
     }
 }
